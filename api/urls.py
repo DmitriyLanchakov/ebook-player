d312a505bbdf53d70898ce69ba4fc3c0b85def30
emeth- <seanybob@gmail.com>
2016-09-10 08:22:34 -0500

    hows about that

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^save_fish$', "api.views.save_fish"),
-    url(r'^delete_fish$', "api.views.delete_fish"),
-    url(r'^get_fish$', "api.views.get_fish"),
+    url(r'^get_book/(?P<book_id>[-&\w]+)$', "api.views.get_book"),
     url(r'^$', "api.views.load_frontend")
 )
--- api/views.py
+++ api/views.py
@@ -3,6 +3,8 @@ import datetime
 import json
 from api.models import Fish
 from django.http import HttpResponseRedirect
+import requests
+from bs4 import BeautifulSoup
 
 def json_custom_parser(obj):
     if isinstance(obj, datetime.datetime) or isinstance(obj, datetime.date):
@@ -11,46 +13,42 @@ def json_custom_parser(obj):
     else:
         raise TypeError(obj)
 
-def delete_fish(request):
-    """
-        Expects as input:
-            - An id located in:
-                - request.body if Angular.js
-                - request.POST['id'] if backbone / jquery
-    """
-    fish_id = request.body #change this depending on how your frontend sends data, as POST or in the body
+def get_book(request, book_id):
 
-    Fish.objects.filter(id=fish_id).delete()
-    return HttpResponse(json.dumps({
-        "status": "success"
-    }, default=json_custom_parser), content_type='application/json', status=200)
+    book_data = requests.get('https://librivox.org/api/feed/audiobooks/?id='+str(book_id)+'&format=json').json()['books'][0]
+    final = {
+        "totaltime": book_data['totaltime'],
+        "totaltimesecs": book_data['totaltimesecs'],
+        "title": book_data['title'],
+        "author": book_data['authors'][0]['first_name'] + ' ' + book_data['authors'][0]['last_name'],
+        "num_sections": book_data['num_sections'],
+        "url_project": book_data['url_project'],
+        "id": book_data['id']
+    }
 
-def save_fish(request):
-    """
-        Expects as input:
-            - A json string location in:
-                - request.body if Angular.js
-                - request.POST['fish_info'] if backbone / jquery
-    """
+    if 'url_librivox' in book_data:
+        resp = requests.get(book_data['url_librivox'])
+        soup = BeautifulSoup(resp.text)
+        all_links = []
+        for link in soup.findAll("a", { "class" : "play-btn" }):
+            if link.has_attr('href'):
+                all_links.append(link['href'])
+                print link['href']
+        final['sections'] = all_links
+        
+    elif 'url_iarchive' in book_data:
+        resp = requests.get('https://archive.org/embed/uncle_toms_cabin_librivox&playlist=1')
+        #resp = requests.get(book_data['url_librivox'])
+        all_links = []
+        for track in json.loads(resp.text.split('play_setup')[1].split("Play('jw6',")[1].split('{"start')[0].strip()[:-1]):
+            for source in track['sources']:
+                if source['type'] == "mp3":
+                    all_links.append("https://archive.org/" + source['file'])
+                    break
+        final['sections'] = all_links
 
-    fish_info = request.body #change this depending on how your frontend sends data, as POST or in the body
+    return HttpResponse(json.dumps(final, default=json_custom_parser), content_type='application/json', status=200)
 
-    fsh = Fish(**json.loads(fish_info))
-    fsh.save()
-    return HttpResponse(json.dumps({
-        "status": "success",
-        "id": fsh.id,
-        "data": list(Fish.objects.filter(id=fsh.id).values())[0] #lololol
-    }, default=json_custom_parser), content_type='application/json', status=200)
 
-def get_fish(request):
-    fishies = Fish.objects.all()
-    return HttpResponse(json.dumps({
-        "status": "success",
-        "data": list(fishies.values())
-    }, default=json_custom_parser), content_type='application/json', status=200)
-    
-    
 def load_frontend(request):
     return HttpResponseRedirect("/static/index.html")
-
